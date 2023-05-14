19a2f5293b60dd508188713ea3c5ae74a93578e9
Bob <zhang.zebedy@gmail.com>
2023-05-14 09:06:23 +0800

    添加人民币大小写转换, 更新词库

---
+++ rime.lua
@@ -0,0 +1,16 @@
+-- 以词定字
+select_character = require("select_character")
+-- 日期时间
+date_translator = require("date_translator")
+-- Unicode
+unicode = require("unicode")
+-- 为用户词典中（输入过）的内容结尾加上一个星号
+is_in_user_dict = require("is_in_user_dict")
+-- v 模式 symbols 优先
+v_filter = require("v_filter")
+-- 降低部分英语单词在候选项的位置
+reduce_english_filter = require("reduce_english_filter")
+-- 长词优先
+long_word_filter = require("long_word_filter")
+-- 人民币大写
+number_translator = require("number_translator")
