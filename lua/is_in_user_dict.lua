5438c5eb68b44d86a921083524f5b61244a53a6a
Bob <zhang.zebedy@gmail.com>
2023-05-27 10:27:18 +0800

    更新is_in_user_dict

--- lua/is_in_user_dict.lua
+++ lua/is_in_user_dict.lua
@@ -1,7 +1,7 @@
 -- 为用户词典的内容结尾加上一个星号 *
 local function is_in_user_dict(input, env)
     for cand in input:iter() do
-        if (string.find(cand.type, "user")) then
+        if cand.type == "user_phrase" then
             cand.comment = cand.comment .. '*'
         end
         yield(cand)
